0674065d8ae42f00dc6a9926c8e3e39d58da17b5
BOUADJENEK Yacine <bouadjenek.yacin@gmail.com>
2021-08-13 20:46:11 +0200

    ajout de la page scanner

---
+++ src/Controller/MouvementStockController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MouvementStockController extends AbstractController
+{
+    /**
+     * @Route("/mouvement-stock", name="mouvement")
+     */
+    public function index(): Response
+    {
+        return $this->render('mouvement_stock/index.html.twig', [
+            'controller_name' => 'MouvementStockController',
+        ]);
+    }
+
+    /**
+     * @Route("/mouvement-stock/entree", name="entree")
+     */
+    public function entree(): Response
+    {
+        
+        return $this->render('mouvement_stock/scanner.html.twig', [
+            
+        ]);
+    }
+
+    /**
+     * @Route("/mouvement-stock/sortie", name="sortie")
+     */
+    public function sortie(): Response
+    {
+        
+        return $this->render('mouvement_stock/scanner.html.twig', [
+            
+        ]);
+    }
+}
